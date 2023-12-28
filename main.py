9cd967b6a336d1c0753d9f03ed5a173525453ddc
Matt Young <youngmatt519@gmail.com>
2023-12-27 20:52:34 -0500

    Added and adjusted visualization methods for function runtimes

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 import math
 import matplotlib.pyplot as plt
 
-
-
-
-
 def f4(n):
     """
     Calculates and returns the log base 2 of a given number. This is for use with Kleinberg/Tardos
@@ -49,12 +45,51 @@ def compare_f4_and_f5():
     plt.legend()
     plt.show()
 
+def graph_log_n(n):
+    x = []
+    y = []
+    for i in range(1, n+1):
+        x.append(i)
+        y.append(f4(i))
+
+    plt.plot(x, y)
+    plt.show()
+
+def graph_f4_and_f5(n):
+    x = []
+    y1 = []
+    y2 = []
+    for i in range(1, n+1):
+        x.append(i)
+        y1.append(f4(i))
+        y2.append(f5(i))
+
+    plt.plot(x, y1, label='f4', color='blue')
+    plt.plot(x, y2, label='f5', color='green')
+    #plt.xscale('log', base=5)
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('f4 vs f5 growth rates')
+    plt.legend()
+    plt.show()
+
+def graph_n_log_n(n):
+    x = []
+    y = []
+    for i in range(1, n+1):
+        x.append(i)
+        y.append(n * math.log(i, 2))
+
+    plt.plot(x, y)
+    plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    compare_f4_and_f5()
-
+    #compare_f4_and_f5()
+     #graph_log_n(500)
+    # graph_n_log_n(500)
+    graph_f4_and_f5(500000)
 
 
 
